c016d70a8759e8b6c5a580aa373c7c1e3b2ce79f
Verunchikb <156007538+Verunchikb@users.noreply.github.com>
2024-01-10 16:56:40 +0500

    Add files via upload

---
+++ src/main/java/Parse/CsvReader.java
@@ -0,0 +1,38 @@
+package Parse;
+
+import Parameters.Grand;
+import Sqlite.Conn;
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.bean.ColumnPositionMappingStrategy;
+import au.com.bytecode.opencsv.bean.CsvToBean;
+
+import java.io.FileReader;
+import java.util.List;
+
+public class CsvReader {
+
+    public static void main(String[] args) throws Exception
+    {
+        CsvToBean csv = new CsvToBean();
+        Conn conn = new Conn();
+        conn.Delete();
+        conn.create();
+        String csvFilename = "src/main/resources/Granty.csv";
+        CSVReader csvReader1 = new CSVReader(new FileReader(csvFilename));
+        List list1 = csv.parse(setColumMapping1(), csvReader1);
+        list1.remove(0);
+        for (Object object : list1) {
+            Grand grand = (Grand) object;
+            try {conn.insert(grand);}
+            catch (Exception e) {}}
+    }
+
+    private static ColumnPositionMappingStrategy setColumMapping1()
+    {
+        ColumnPositionMappingStrategy strategy = new ColumnPositionMappingStrategy();
+        strategy.setType(Grand.class);
+        String[] columns = new String[] {"company","street", "money","year","type","places"};
+        strategy.setColumnMapping(columns);
+        return strategy;
+    }
+}
